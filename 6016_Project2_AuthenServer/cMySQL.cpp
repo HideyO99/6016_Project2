7fb8d41ea9df3a3b1d35115f8d77d9fcd49db3c0
Siraphong <hideyo99@hotmail.com>
2022-11-24 02:16:25 -0500

    add sha256 lib, createNewAccount, userAuthen

--- 6016_Project2_AuthenServer/cMySQL.cpp
+++ 6016_Project2_AuthenServer/cMySQL.cpp
@@ -3,6 +3,8 @@
 
 cMySQL::cMySQL()
 {
+	Salt_.clear();
+	Hash_.clear();
 	pDriver = nullptr;
 	pConnection = nullptr;
 	pResultSet = nullptr;
@@ -62,4 +64,95 @@ void cMySQL::Disconnect()
 	delete pResultSet;
 	delete pInsertStatement;
 
+}
+
+void cMySQL::genSalt(int length)
+{
+	std::string Salt;
+	for (size_t i = 0; i < length; i++)
+	{
+		char c = (rand() % 2 == 1) ? 'a' : 'A';
+		c += rand() % 26;
+		Salt += c;
+	}
+	Salt_ = Salt;
+}
+
+void cMySQL::calHash(std::string input)
+{
+	SHA256 sha256;
+	Hash_ = sha256(Salt_ + input).c_str();
+}
+
+bool cMySQL::createNewAccount(std::string email,std::string passwd)
+{
+	//Connect();
+	genSalt(passwd.length());
+	calHash(passwd.c_str());
+	try
+	{
+		pInsertStatement = pConnection->prepareStatement("INSERT INTO web_auth (email, salt, hashed_password) VALUES (?,?,?);");
+
+	}
+	catch (sql::SQLException e)
+	{
+		std::cerr << "cannot create new account: " << e.what() << std::endl;
+		return false;
+	}
+
+	pInsertStatement->setString(1, email);
+	pInsertStatement->setString(2, Salt_);
+	pInsertStatement->setString(3, Hash_);
+	
+	try
+	{
+		pInsertStatement->execute();
+	}
+	catch (sql::SQLException e)
+	{
+		std::cerr << "cannot execute create new account: " << e.what() << std::endl;
+	}
+
+	try
+	{
+		pStatement = pConnection->createStatement();
+		pStatement->execute("INSERT INTO user (creation_date) VALUES (now()); ");
+	}
+	catch (sql::SQLException e)
+	{
+		std::cerr << "cannot insert creation date: " << e.what() << std::endl;
+	}
+
+	Salt_.clear();
+	Hash_.clear();
+	//Disconnect();
+
+	return true;
+}
+
+bool cMySQL::userAuthen(std::string email, std::string passwd)
+{
+	//Connect();
+	try
+	{
+		std::string q = "SELECT salt, hashed_password, userID FROM authserver WHERE email = " + email;
+		pStatement = pConnection->createStatement();
+		pResultSet = pStatement->executeQuery(q);
+		Salt_.clear();
+		Salt_ = pResultSet->getString("salt");
+		calHash(passwd.c_str());
+		if (Hash_.compare(pResultSet->getString("hashed_password")) != 0)
+		{
+			std::cout << "incorrect password" << std::endl;
+		}
+		std::string u = "UPDATE user SET last_login = now() WHERE id = " + pResultSet->getString("userID");
+		pStatement = pConnection->createStatement();
+		pStatement->executeUpdate(u);
+	}
+	catch (sql::SQLException e)
+	{
+		std::cerr << "cannot login : " << e.what() << std::endl;
+		return false;
+	}
+	return true;
 }
--- 6016_Project2_AuthenServer/cMySQL.h
+++ 6016_Project2_AuthenServer/cMySQL.h
@@ -6,12 +6,13 @@
 #include "jdbc/cppconn/statement.h"
 #include "jdbc/cppconn/prepared_statement.h"
 #include "jdbc/cppconn/resultset.h"
-#include <hash-library/sha256.h>
+#include "hash-library/sha256.h"
 
 #define HOST		"127.0.0.1:3306"
 #define USERNAME	"yo"
 #define PASSWD		"1234"
 #define SCHEMA		"authserver"
+
 class cMySQL
 {
 public:
@@ -21,10 +22,14 @@ public:
 	bool Connect();
 	void Disconnect();
 	
-	void genSalt();
-	void calHash();
+	void genSalt(int length);
+	void calHash(std::string input);
+	bool createNewAccount(std::string email, std::string passwd);
+	bool userAuthen(std::string email, std::string passwd);
 
 private:
+	std::string Salt_;
+	std::string Hash_;
 	sql::Driver* pDriver;
 	sql::Connection* pConnection;
 	sql::ResultSet* pResultSet;
